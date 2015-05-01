40e9ec3c4bbca03793d9bcc0eb2405c745f373ef
sheepguo <352414710@qq.com>
2015-05-01 16:54:18 +0800

    增加了hello NJU

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, world!");	
-				System.out.println("Hello, world!");	
+		System.out.println("Hello, China!");	
+		System.out.println("Hello, NJU!");	
 
 	}
 }
